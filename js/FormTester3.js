eca7433a1541c8985b519b5af3e27c8acec491a8
zhongpuwu <47552333+zhongpuwu@users.noreply.github.com>
2019-10-02 12:01:06 +0800

    1

--- js/FormTester3.js
+++ js/FormTester3.js
@@ -3,11 +3,13 @@ var more = '<img src="/image3/more.png">';
 var right = '<img src="/image3/right.png">';
 var question = '<img src="/image3/question.png">';
 
+//检验用户名
 function validate_username(id) {
+    var usernameReg = /^[a-zA-Z0-9][a-zA-Z0-9_-]{2,16}[a-zA-Z0-9]$/;
     if (id == "") {
         document.getElementById("warning_id").innerHTML = wrong;
         document.getElementById("warning_id").innerHTML += '<label>用户名不能为空</label>';
-    } else if (id == "1") {
+    } else if (id.search(usernameReg) == -1) {
         document.getElementById("warning_id").innerHTML = wrong;
         document.getElementById("warning_id").innerHTML += '<label>1.用户名由字母、数字、下划线、减号组成</label><br>';
         document.getElementById("warning_id").innerHTML += '<label>2.用户名只能以数字、字母开头或结尾</label><br>';
@@ -15,24 +17,56 @@ function validate_username(id) {
     } else document.getElementById("warning_id").innerHTML = right;
 }
 
+// '<label></label><br>'
+// 检验密码
+function validate_password(password) {
+    var passwordReg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,}$/;
+    if (password == "") {
+        document.getElementById("warning_password").innerHTML = wrong;
+        document.getElementById("warning_password").innerHTML += '<label>密码不能为空</label>';
+    } else if (password.search(passwordReg) == -1) {
+        document.getElementById("warning_password").innerHTML = wrong;
+        document.getElementById("warning_password").innerHTML += '<label>密码长度须大于6</label><br>';
+        document.getElementById("warning_password").innerHTML += '<label>至少包括大小写字母和数字</label>';
+    } else document.getElementById("warning_password").innerHTML = right;
+}
+
+// 检验第二次密码
+function validate_password2(password2) {
+    var password = document.getElementById("orign_password");
+    if (password2 != password.value) {
+        document.getElementById("warning_password2").innerHTML = wrong;
+        document.getElementById("warning_password2").innerHTML += '<label>两次密码输入不一致</label>';
+    } else document.getElementById("warning_password2").innerHTML = right;
+}
+
+// 检验手机号
+function validate_Pnumber(Pnumber) {
+    var PnumberReg = /^[1][0-9]{10}$/;
+    if (Pnumber == ""){
+        document.getElementById("warning_Pnumber").innerHTML = wrong;
+        document.getElementById("warning_Pnumber").innerHTML += '<label>手机号不能为空</label>';
+    } else if(Pnumber.search(PnumberReg)==-1){
+        document.getElementById("warning_Pnumber").innerHTML = wrong;
+        document.getElementById("warning_Pnumber").innerHTML += '<label>手机号格式不正确</label>';
+    } else   document.getElementById("warning_Pnumber").innerHTML = right;
+}
+
+
+
+// 再次点下时清空标记
 function validate_clear_id() {
     document.getElementById("warning_id").innerHTML = "";
 }
+
 function validate_clear_password() {
     document.getElementById("warning_password").innerHTML = "";
 }
+
 function validate_clear_password2() {
     document.getElementById("warning_password2").innerHTML = "";
 }
 
-function validate_password(password) {
-    //^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6-10}$		
-    //测试密码：12345y  		
-    var passwordReg = /^[0-9][a-zA-Z][0-9A-Za-z]{6-10}$/;
-    if (password != "" && password.search(passwordReg) != -1) {
-        document.getElementById("warning_password").innerHTML = "<font color='green' size='3px'>√密码格式正确</font>";
-    } else {
-        document.getElementById("warning_password").innerHTML = "<font color='red' size='3px'>密码格式错误</font>";
-        alert("密码有6位，由数字和字母组成!");
-    }
+function validate_clear_Pnumber() {
+    document.getElementById("warning_Pnumber").innerHTML = "";
 }
