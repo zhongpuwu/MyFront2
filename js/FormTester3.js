83ddad5b9da6996b67a5d063a4d4b0e2de26dbba
zhongpuwu <47552333+zhongpuwu@users.noreply.github.com>
2019-10-07 00:56:29 +0800

    太难了

--- js/FormTester3.js
+++ js/FormTester3.js
@@ -8,12 +8,12 @@ function validate_username(id) {
     var usernameReg = /^[a-zA-Z0-9][a-zA-Z0-9_-]{2,16}[a-zA-Z0-9]$/;
     if (id == "") {
         document.getElementById("warning_id").innerHTML = wrong;
-        document.getElementById("warning_id").innerHTML += '<label>用户名不能为空</label>';
+        document.getElementById("warning_id").innerHTML += '<label>&nbsp;用户名不能为空</label>';
     } else if (id.search(usernameReg) == -1) {
         document.getElementById("warning_id").innerHTML = wrong;
-        document.getElementById("warning_id").innerHTML += '<label>1.用户名由字母、数字、下划线、减号组成</label><br>';
-        document.getElementById("warning_id").innerHTML += '<label>2.用户名只能以数字、字母开头或结尾</label><br>';
-        document.getElementById("warning_id").innerHTML += '<label>3.用户名长度为 4-18 字符</label>';
+        document.getElementById("warning_id").innerHTML += '<label>&nbsp;1.用户名由字母、数字、下划线、减号组成</label><br>';
+        document.getElementById("warning_id").innerHTML += '<label>&nbsp;2.用户名只能以数字、字母开头或结尾</label><br>';
+        document.getElementById("warning_id").innerHTML += '<label>&nbsp;3.用户名长度为 4-18 字符</label>';
     } else document.getElementById("warning_id").innerHTML = right;
 }
 
@@ -23,11 +23,11 @@ function validate_password(password) {
     var passwordReg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,}$/;
     if (password == "") {
         document.getElementById("warning_password").innerHTML = wrong;
-        document.getElementById("warning_password").innerHTML += '<label>密码不能为空</label>';
+        document.getElementById("warning_password").innerHTML += '<label>&nbsp;密码不能为空</label>';
     } else if (password.search(passwordReg) == -1) {
         document.getElementById("warning_password").innerHTML = wrong;
-        document.getElementById("warning_password").innerHTML += '<label>密码长度须大于6</label><br>';
-        document.getElementById("warning_password").innerHTML += '<label>至少包括大小写字母和数字</label>';
+        document.getElementById("warning_password").innerHTML += '<label>&nbsp;密码长度须大于6</label><br>';
+        document.getElementById("warning_password").innerHTML += '<label>&nbsp;至少包括大小写字母和数字</label>';
     } else document.getElementById("warning_password").innerHTML = right;
 }
 
@@ -36,22 +36,45 @@ function validate_password2(password2) {
     var password = document.getElementById("orign_password");
     if (password2 != password.value) {
         document.getElementById("warning_password2").innerHTML = wrong;
-        document.getElementById("warning_password2").innerHTML += '<label>两次密码输入不一致</label>';
+        document.getElementById("warning_password2").innerHTML += '<label>&nbsp;两次密码输入不一致</label>';
     } else document.getElementById("warning_password2").innerHTML = right;
 }
 
 // 检验手机号
 function validate_Pnumber(Pnumber) {
     var PnumberReg = /^[1][0-9]{10}$/;
-    if (Pnumber == ""){
+    if (Pnumber == "") {
         document.getElementById("warning_Pnumber").innerHTML = wrong;
-        document.getElementById("warning_Pnumber").innerHTML += '<label>手机号不能为空</label>';
-    } else if(Pnumber.search(PnumberReg)==-1){
+        document.getElementById("warning_Pnumber").innerHTML += '<label>&nbsp;手机号不能为空</label>';
+    } else if (Pnumber.search(PnumberReg) == -1) {
         document.getElementById("warning_Pnumber").innerHTML = wrong;
-        document.getElementById("warning_Pnumber").innerHTML += '<label>手机号格式不正确</label>';
-    } else   document.getElementById("warning_Pnumber").innerHTML = right;
+        document.getElementById("warning_Pnumber").innerHTML += '<label>&nbsp;手机号格式不正确</label>';
+    } else document.getElementById("warning_Pnumber").innerHTML = right;
 }
 
+// 发送验证码开始计时
+function sendCode(button) {
+    time(button);
+
+}
+
+//验证码倒计时
+var wait = 59;
+function time(button) {
+    if (wait == 0) {
+        button.removeAttribute("disabled");
+        button.value = "获取验证码";
+        wait = 59;
+    } else {
+        button.setAttribute("disabled", true);
+        button.value = wait + "秒后重新获取";
+        wait--;
+        setTimeout(function () {
+            time(button)
+        },
+            1000)
+    }
+}
 
 
 // 再次点下时清空标记
@@ -69,4 +92,8 @@ function validate_clear_password2() {
 
 function validate_clear_Pnumber() {
     document.getElementById("warning_Pnumber").innerHTML = "";
+}
+
+function validate_clear_code() {
+    document.getElementById("check_Code").innerHTML = "";
 }
